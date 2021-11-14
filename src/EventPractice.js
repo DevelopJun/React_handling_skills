5ac4c9d255e924a9fd24e2c03e761c1c87cb47d9
june <llovejune@nate.com>
2021-11-14 15:14:11 +0900

    property initializer syntax를 사용한 메서드 작성

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    message: '',
+  };
+  // 바벨의 transform-class-properties 문법을 사용하여 화살표 함수 형태로 메서드를 정의하는게 좋을 수 도 있다.
+  // 이렇게 만드는 것이 훨씬 깔끔하다.
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: '',
+    });
+  };
+
   render() {
     return (
       <div>
@@ -9,10 +27,10 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해보세요"
-          onChange={(e) => {
-            console.log(e);
-          }}
-        ></input>
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
