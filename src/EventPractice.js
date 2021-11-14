9658bdd32e98aea605b987ddbcea9651e76e0936
june <llovejune@nate.com>
2021-11-14 15:28:17 +0900

    onkeypress

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,20 +3,30 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
   state = {
     message: '',
+    username: '',
   };
   // 바벨의 transform-class-properties 문법을 사용하여 화살표 함수 형태로 메서드를 정의하는게 좋을 수 도 있다.
   // 이렇게 만드는 것이 훨씬 깔끔하다.
+
+  // 여기 코드가 핵심이다.
   handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + ': ' + this.state.message);
     this.setState({
+      username: '',
       message: '',
     });
+  };
+  // Enter 쳤을때 바로 handleclick 나올 수 있도록
+  handlekeypress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleClick();
+    }
   };
 
   render() {
@@ -25,11 +35,20 @@ class EventPractice extends Component {
         <h1> 이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handlekeypress}
         />
+
         <button onClick={this.handleClick}>확인</button>
       </div>
     );
