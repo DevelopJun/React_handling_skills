a7cf9b82b34670bf4be3a33bbd7852e98684285d
june <llovejune@nate.com>
2021-11-14 15:45:14 +0900

    event handling finish

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,58 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-  state = {
-    message: '',
-    username: '',
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setUsername('');
+    setMessage('');
   };
-  // 바벨의 transform-class-properties 문법을 사용하여 화살표 함수 형태로 메서드를 정의하는게 좋을 수 도 있다.
-  // 이렇게 만드는 것이 훨씬 깔끔하다.
-
-  // 여기 코드가 핵심이다.
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleClick = () => {
-    alert(this.state.username + ': ' + this.state.message);
-    this.setState({
-      username: '',
-      message: '',
-    });
-  };
-  // Enter 쳤을때 바로 handleclick 나올 수 있도록
-  handlekeypress = (e) => {
+  const onkeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.handleClick();
+      onClick();
     }
   };
-
-  render() {
-    return (
-      <div>
-        <h1> 이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해보세요"
-          value={this.state.message}
-          onChange={this.handleChange}
-          onKeyPress={this.handlekeypress}
-        />
-
-        <button onClick={this.handleClick}>확인</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="사용자명"
+        value={message}
+        onChange={onChangeMessage}
+        onkeyPress={onkeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
